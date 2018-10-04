a0dd7c2979175e68c2dd92993c546b8533f37256
Denis Babuskin <3169477@gmail.com>
2018-10-04 21:36:27 +0300

    add function of solving sudoku

--- src/index.js
+++ src/index.js
@@ -1,136 +1,104 @@
 module.exports = function solveSudoku(matrix) {
-  //create array for transformation and will work with it
-  const newMatrix = matrix;
-
-  // check row
-  function checkRow(row, column, value) {
-    let s = 0;
-    for (let i = 0; i < 9; i++) {
-      if (i == column) {
-        continue;
-      }
-      if (value == newMatrix[row][i]) {
-        s++;
-      }
-    }
-    if (s == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // check column
-  function checkColumn(row, column, value) {
-    let s = 0;
-    for (let i = 0; i < 9; i++) {
-      if (i == row) {
-        continue;
-      }
-      if (value == newMatrix[i][column]) {
-        s++;
-      }
-    }
-    if (s == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //get box values
-  function getBox(value) {
-    switch (value) {
-      case 0: return [0,2];
-      case 1: return [0,2];
-      case 2: return [0,2];
-      case 3: return [3,5];
-      case 4: return [3,5];
-      case 5: return [3,5];
-      case 6: return [6,8];
-      case 7: return [6,8];
-      case 8: return [6,8];
-    }
-  }
-
-  // check box 3x3
-  function checkBox([startR, endR], [startC, endC], row, column, value) {
-    let s = 0;
-    for (let i = startR; i <= endR; i++) {
-      for (let j = startC; j <= endC; j++) {
-        if (i == row && j == column) {
-          continue;
-        }
-        if (value == newMatrix[i][j]) {
-          s++;
-        }
-      }
-    }
-    if (s == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // check value function (combine row, column and box check)
-  function checkValue(row, column, value) {
-    if (checkRow(row, column, value) && checkColumn(row, column, value) && checkBox(getBox(row), getBox(column), row, column, value)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  //get emptyArray - array with unknown values from given Sudoku
-  function getEmptyArray(ar) {
-    let a = [];
-    let i, j;
-    for (i = 0; i < 9; i++) {
-      for (j = 0; j < 9; j++) {
+  //save empty positions to work only with them
+  function emptyPos (ar) {
+    let newMatrix = [];
+    for (let i = 0; i < ar.length; i++) {
+      for (let j = 0; j < ar[0].length; j++) {
         if (ar[i][j] === 0) {
-          a.push(i, j);
+          newMatrix.push([i, j]);
         }
       }
     }
-    return a;
+    return newMatrix;
   }
 
-  //variables
-  let emAr = getEmptyArray(newMatrix);
-  let row, col, val, s;
+  //checkRow
+  function checkRow (row, col, val, ar) {
+    for (let i = 0; i < ar[row].length; i++) {
+      if (i === col) continue;
+      if (val === ar[row][i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  //solving the main task
-  for (let i = 0; i < (emAr.length / 2); i++) {
-    row = emAr[i*2];
-    col = emAr[i*2+1];
-    val = newMatrix[row][col];
-    s = val;
+  //checkColumn
+  function checkColumn (row, col, val, ar) {
+    for (let i = 0; i < ar.length; i++) {
+      if (i === row) continue;
+      if (val === ar[i][col]) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    // try to rewrite this for to while cycle
-    if (val < 9) {
-      do {
-        val++;
-        if (checkValue(row, col, val)) {
-          newMatrix[row][col] = val;
-          s = 0;
-          break;
-        } else {
-          s++;
+  //getBoxBorder
+  function boxBorder (a) {
+    switch (a) {
+      case 0: return [0, 2];
+      case 1: return [0, 2];
+      case 2: return [0, 2];
+      case 3: return [3, 5];
+      case 4: return [3, 5];
+      case 5: return [3, 5];
+      case 6: return [6, 8];
+      case 7: return [6, 8];
+      case 8: return [6, 8];
+      default: return "whole sheet, bug";
+    }
+  }
+
+  //check3x3
+  function check3x3 (row, col, val, ar) {
+    let sR = boxBorder(row)[0],
+        eR = boxBorder(row)[1],
+        sC = boxBorder(col)[0],
+        eC = boxBorder(col)[1];
+    for (let i = sR; i <= eR; i++) {
+      for (let j = sC; j <= eC; j++) {
+        if (i === row && j === col) continue;
+        if (ar[i][j] === val) {
+          return false;
         }
-      } while (val < 9);
-    } else {
-      newMatrix[row][col] = 0;
-      i = i - 2;
+      }
     }
+    return true;
+  }
 
-    // Check if value is not fit to the place. Return to the previous
-    // step if not
-    if (s == 9) {
-      newMatrix[row][col] = 0;
-      i = i - 2;
+  //checkValue
+  function checkValue (row, col, val, ar) {
+    if (checkRow(row, col, val, ar) && checkColumn(row, col, val, ar) && check3x3(row, col, val, ar)) {
+      return true;
+    } else {
+      return false;
     }
   }
 
-  return(newMatrix);
+  //solveSudoku
+  let emptyArray = emptyPos(matrix);
+  let solveMatrix = matrix;
+  let val, row, col, er, i;
+  for (i = 0; i < emptyArray.length; i ++) {
+    row = emptyArray[i][0];
+    col = emptyArray[i][1];
+    val = Math.floor(solveMatrix[row][col]);
+    if (val < 9) {
+      for (let j = val; j < 9; j++) {
+        val++;
+        if (checkValue(row, col, val, solveMatrix)) {
+          solveMatrix[row][col] = val;
+          break;
+        } else if (val == 9) {
+            solveMatrix[row][col] = 0;
+            i = i - 2;
+        }
+      }
+    } else {
+      solveMatrix[row][col] = 0;
+      i = i - 2;
+    }
+  }
+  return solveMatrix;
 }
